2a088fdc9261b9b93350fc4f1c1b8072e23ad237
spoonman1982 <farrellyjames@yahoo.ie>
2015-09-30 12:04:38 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Canteen2020::Application.config.secret_key_base = '386286cbb63d8c55c15c9c8bdc005d7cac994d1ad829bf3f3677a28cfaa990ae3ee2b5e7cc90ad69d995788d51a4e78993d7ef5796656ba73fdd9638a6c9077f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Canteen2020::Application.config.session_store :cookie_store, key: '_canteen2020_session'
